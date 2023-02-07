db9146db579793d657b536f22f815d8602ee4d9a
alicelazzeri <alice.lazzeri@hotmail.it>
2023-02-07 09:23:02 +0100

    changes added

---
+++ s2/g1/notes/basi-js/js/script.js
@@ -0,0 +1,10 @@
+//commento su una riga singola, se vado a capo, sono fuoroi dal commento
+
+/*
+commento su più righe
+*/
+
+// alert("Hello, world!"); trasformo l'alter in commento per disabilitarlo
+
+//il punto e virgola un tempo era obbligatorio, adesso è opzionale e indica la fine del commento
+
